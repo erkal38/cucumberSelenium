49914b9b4fa50cf2f67ddc3065ee619b2f56bcad
erkal38 <nalruvceysem1984@gmail.com>
2020-07-13 19:17:33 +0300

    cucumber update

--- src/test/java/com/vytrack/step_definitions/Hooks.java
+++ src/test/java/com/vytrack/step_definitions/Hooks.java
@@ -8,6 +8,7 @@ import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 
 public class Hooks {
+    //test final
     @Before
     public void setUp(){
         System.out.println("\tthis is coming from Before");
